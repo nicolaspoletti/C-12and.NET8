682ce5458d1c4e3fdcb4ee9cb2a58caf11743086
Nicolas Poletti <nicolaspoletti1@gmail.com>
2024-04-30 17:41:34 +1000

    Chapter 1 of C# 12 and .NET 8 - Modern Cross-Platform Development Fundamentals

---
+++ AboutMyEnvironment/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AboutMyEnvironment;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine(Environment.CurrentDirectory);
+        System.Console.WriteLine(Environment.OSVersion.VersionString);
+        System.Console.WriteLine("Namespace: {0}", typeof(Program).Namespace);
+    }
+}
